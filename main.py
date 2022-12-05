aadeb3b978ebf03e8a53fd3c170869dd7d40347f
Monique Saviczki <monique_saviczki@hotmail.com>
2022-12-05 04:15:34 -0300

    create main

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import sys
 from PyQt5 import uic, QtWidgets
 
+def confereSenha(passaword, confPassword):
+    if passaword == confPassword:
+        return
+    else:
+        print("As duas senhas devem ser iguais")
+
+def cadastrar():
+    cadastro.show()
+    cadastro.pushButton.clicked.connect(voltar)
+
+def voltar():
+    cadastro.close()
+
+def entrar():
+    chat.show()
+
 app = QtWidgets.QApplication(sys.argv)
 login = uic.loadUi("login.ui")
 cadastro = uic.loadUi("cadastro.ui")
 chat = uic.loadUi("chat.ui")
 
+login.pushButton.clicked.connect(cadastrar)
+login.pushButton_2.clicked.connect(entrar)
 
-chat.show()
+
+
+login.show()
 app.exec_()
