0c8c68d6bf6f31658f546653cfc531b84e7721cf
Weiry <gonweiry@gmail.com>
2022-12-13 03:35:03 +0000

    Enviando dois parametros e salvando no servidor

--- client.py
+++ client.py
@@ -1,9 +1,41 @@
-import Pyro4
+from Pyro4 import Daemon, Proxy, expose, oneway, callback, locateNS
+import threading, time
+@expose
+class Cliente(object):
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
+        self.uri = ""
 
-ns = Pyro4.locateNS()
+    def get_nome(self):
+        return self.nome
 
-uri = ns.lookup('obg')
+    def get_senha(self):
+        return self.senha
+    
+    def get_uri(self):
+        return self.uri
 
-o = Pyro4.Proxy(uri)
+    def request_loop(self, daemon):
+        daemon.requestLoop()
+        time.sleep(2)
+    
 
-print(o)
+with Daemon() as daemon:
+    
+    nome = input("Digite seu nome: ")
+    senha = input("Digite sua senha: ")
+
+
+    callback = Cliente(nome, senha)
+    callback.uri = daemon.register(callback)
+
+    loop_thread = threading.Thread(target=callback.request_loop, args=(daemon, ))
+    loop_thread.daemon = False
+    loop_thread.start()
+
+        
+    with Proxy("PYRONAME:RMI") as server:
+        # Cadastro do usuário
+        server.cadastrar_cliente(callback.uri)
+        server.show_users()
--- server.py
+++ server.py
@@ -1,19 +1,39 @@
-import Pyro4
-
-@Pyro4.expose
-class Hello:
-    def say(self):
-        return 'Hello'
+from Pyro4 import Daemon, Proxy, expose, oneway, callback, locateNS
+import threading
 
 
-daemon = Pyro4.Daemon()
+class Usuario():
 
-uri = daemon.register(Hello) # Regitra a class
+    def __init__(self, nome, senha, uri) -> None:
+        self.nome = nome
+        self.senha = senha
+        self.uri = uri
+        print(f"Usuario {nome} criado")
 
-ns = Pyro4.locateNS()   #Localiza endereço do server
 
-ns.register('obg', uri)
 
-print(uri)
+@expose
+class Servidor(object):
+    usuarios = []
 
-daemon.requestLoop()
+    def cadastrar_cliente(self, callback):
+        cliente = Proxy(callback)
+        usuario = Usuario(cliente.get_nome(), cliente.get_senha(),callback)
+        Servidor.usuarios.append(usuario)
+
+    def show_users(self, users = usuarios):
+        for user in users:
+            print(user.nome, user.senha)
+
+
+with Daemon() as daemon:
+    print("Starting server")
+    ns = locateNS()
+    server = Servidor()
+    uri = daemon.register(server)
+    ns.register("RMI", uri)
+
+    print(uri)
+
+    daemon.requestLoop()
+    
