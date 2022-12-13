68050adc6ba09df92ad35cd38133be1f5126f570
Monique Saviczki <monique_saviczki@hotmail.com>
2022-12-13 02:07:25 -0300

    new ui

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
+import sys, Pyro5
 from PyQt5 import uic, QtWidgets
+from hashlib import sha256
+# senha = 
+# hash = sha256(senha.encode())
+# senha_hash = hash.digest()
 
 def confereSenha(passaword, confPassword):
     if passaword == confPassword:
@@ -9,21 +13,27 @@ def confereSenha(passaword, confPassword):
 
 def cadastrar():
     cadastro.show()
-    cadastro.pushButton.clicked.connect(voltar)
+    cadastro.pushButton_2.clicked.connect(cadastrar)
 
 def voltar():
     cadastro.close()
+    chat.close()
 
 def entrar():
     chat.show()
+    chat.pushButton_2.clicked.connect(voltar)
 
 app = QtWidgets.QApplication(sys.argv)
 login = uic.loadUi("login.ui")
 cadastro = uic.loadUi("cadastro.ui")
 chat = uic.loadUi("chat.ui")
 
-login.pushButton.clicked.connect(cadastrar)
-login.pushButton_2.clicked.connect(entrar)
+
+login.pushButton.clicked.connect(entrar)
+login.pushButton_2.clicked.connect(cadastrar)
+
+#login.lineEdit.text()
+#login.lineEdit_2.text()
 
 
 
