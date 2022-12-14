0207d67ca0e804110d243f7d90dd35d8017a71ef
Monique Saviczki <monique_saviczki@hotmail.com>
2022-12-14 20:37:35 -0300

    class for screens

--- view/main.py
+++ view/main.py
@@ -15,67 +15,67 @@ class Login(QDialog):
         self.pushButton_2.clicked.connect(self.cadastrarClient)
 
     def logIn(self): #Efetua o login
-                    nome = self.lineEdit.text()
-                    senha = self.lineEdit_2.text()
+        nome = self.lineEdit.text()
+        senha = self.lineEdit_2.text()
 
     def cadastrarClient(self): #Direcionara para o cadastro
-                    createClient = Cadastrar()
-                    widget.addWidget(createClient)
-                    widget.setCurrentIndex(widget.currentIndex()+1)
-                    #server.show_users()
+        createClient = Cadastrar()
+        widget.addWidget(createClient)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        #server.show_users()
                     
                 
 class Cadastrar(QDialog):
     def __init__(self):
-                    super(Cadastrar,self).__init__()
-                    loadUi("../view/cadastro.ui",self)
-                    self.pushButton.clicked.connect(self.createClientFunction)
-                    self.pushButton_2.clicked.connect(self.voltar)
+        super(Cadastrar,self).__init__()
+        loadUi("../view/cadastro.ui",self)
+        self.pushButton.clicked.connect(self.createClientFunction)
+        self.pushButton_2.clicked.connect(self.voltar)
 
     
     def createClientFunction(self): #Irá efetuar o cadastro
-                    nome = self.lineEdit.text()
-                    if self.lineEdit_2.text() == self.lineEdit_3.text():
-                        senha = self.lineEdit_2.text()
-                        senha = md5(senha.encode())
+        nome = self.lineEdit.text()
+        if self.lineEdit_2.text() == self.lineEdit_3.text():
+            senha = self.lineEdit_2.text()
+            senha = md5(senha.encode())
 
-                        senha = senha.hexdigest()
+            senha = senha.hexdigest()
 
-                        callback = Cliente(nome, senha)
-                        callback.uri = daemon.register(callback)
+            callback = Cliente(nome, senha)
+            callback.uri = daemon.register(callback)
 
-                        loop_thread = threading.Thread(target=callback.request_loop, args=(daemon, ))
-                        loop_thread.daemon = False
-                        loop_thread.start()
+            loop_thread = threading.Thread(target=callback.request_loop, args=(daemon, ))
+            loop_thread.daemon = False
+            loop_thread.start()
 
-                        server.cadastrar_cliente(callback.uri)
-                        QMessageBox.about(self, "Sucess", "Cliente cadastrado com sucesso!")
+            server.cadastrar_cliente(callback.uri)
+            QMessageBox.about(self, "Sucess", "Cliente cadastrado com sucesso!")
                         
-                    else:
-                        QMessageBox.about(self, "Error", "As senhas devem ser iguais!")
+        else:
+            QMessageBox.about(self, "Error", "As senhas devem ser iguais!")
 
                 
     
     def voltar(self):
-                    login = Login()
-                    widget.addWidget(login)
-                    widget.setCurrentIndex(widget.currentIndex()+1)
+        login = Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
             
 
 class Chatbox(QDialog):
     def __init__(self):
-                    super(Chatbox,self).__init__()
-                    loadUi("../view/chat.ui",self)
-                    self.pushButton.clicked.connect(self.message)
-                    self.pushButton_2.clicked.connect(self.LogOut)
+        super(Chatbox,self).__init__()
+        loadUi("../view/chat.ui",self)
+        self.pushButton.clicked.connect(self.message)
+        self.pushButton_2.clicked.connect(self.LogOut)
 
     def message(self):
-                    self.chatBox.setText(self.lineEdit.text())
+        self.chatBox.setText(self.lineEdit.text())
                 
     def logOut(self):
-                    login = Login()
-                    widget.addWidget(login)
-                    widget.setCurrentIndex(widget.currentIndex()+1)
+        login = Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
 app=QApplication(sys.argv)
 mainwindow=Login()
