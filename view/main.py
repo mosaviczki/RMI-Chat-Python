9b219ec9295a933c990b8b28c5c134096e57c487
Monique Saviczki <monique_saviczki@hotmail.com>
2022-12-27 15:18:16 -0300

    modification in view

--- view/main.py
+++ view/main.py
@@ -1,41 +1,87 @@
-import sys, Pyro4
-from PyQt5 import uic, QtWidgets
-from hashlib import sha256
-# senha = 
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
+from PyQt5.uic import loadUi
+from hashlib import md5
+# senha =
 # hash = sha256(senha.encode())
 # senha_hash = hash.digest()
 
-def confereSenha(passaword, confPassword):
-    if passaword == confPassword:
-        return
-    else:
-        print("As duas senhas devem ser iguais")
 
-def cadastrar():
-    cadastro.show()
-    cadastro.pushButton_2.clicked.connect(cadastrar)
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("../view/login.ui", self)
+        self.pushButton.clicked.connect(self.logIn)
+        self.pushButton_2.clicked.connect(self.cadastrarClient)
 
-def voltar():
-    cadastro.close()
-    chat.close()
+    def logIn(self): #Efetua o login
+        nome = self.lineEdit.text()
+        senha = self.lineEdit_2.text()
 
-def entrar():
-    chat.show()
-    chat.pushButton_2.clicked.connect(voltar)
+    def cadastrarClient(self): #Direcionara para o cadastro
+        createClient = Cadastrar()
+        widget.addWidget(createClient)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        #server.show_users()
+                    
+                
+class Cadastrar(QDialog):
+    def __init__(self):
+        super(Cadastrar,self).__init__()
+        loadUi("../view/cadastro.ui",self)
+        self.pushButton.clicked.connect(self.createClientFunction)
+        self.pushButton_2.clicked.connect(self.voltar)
 
-app = QtWidgets.QApplication(sys.argv)
-login = uic.loadUi("login.ui")
-cadastro = uic.loadUi("cadastro.ui")
-chat = uic.loadUi("chat.ui")
+    
+    def createClientFunction(self): #Irá efetuar o cadastro
+        nome = self.lineEdit.text()
+        if self.lineEdit_2.text() == self.lineEdit_3.text():
+            senha = self.lineEdit_2.text()
+            senha = md5(senha.encode())
 
+            senha = senha.hexdigest()
 
-login.pushButton.clicked.connect(entrar)
-login.pushButton_2.clicked.connect(cadastrar)
+            callback = Cliente(nome, senha)
+            callback.uri = daemon.register(callback)
 
-#login.lineEdit.text()
-#login.lineEdit_2.text()
+            loop_thread = threading.Thread(target=callback.request_loop, args=(daemon, ))
+            loop_thread.daemon = False
+            loop_thread.start()
 
+            server.cadastrar_cliente(callback.uri)
+            QMessageBox.about(self, "Sucess", "Cliente cadastrado com sucesso!")
+                        
+        else:
+            QMessageBox.about(self, "Error", "As senhas devem ser iguais!")
 
+                
+    
+    def voltar(self):
+        login = Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+            
 
-login.show()
+class Chatbox(QDialog):
+    def __init__(self):
+        super(Chatbox,self).__init__()
+        loadUi("../view/chat.ui",self)
+        self.pushButton.clicked.connect(self.message)
+        self.pushButton_2.clicked.connect(self.LogOut)
+
+    def message(self):
+        self.chatBox.setText(self.lineEdit.text())
+                
+    def logOut(self):
+        login = Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(1900)
+widget.setFixedHeight(1024)
+widget.show()
 app.exec_()
